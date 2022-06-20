d616d30d0941fc987552b9d651006a4d7a02d644
h0225w <gusdn5387@naver.com>
2022-06-21 00:14:52 +0900

    로그인 & 회원가입 UI 구현

---
+++ instagramClone/ViewControllers/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  instagramClone
+//
+//  Created by 박현우 on 2022/06/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - "사용자 이름 만들기"로 이동
+    @IBAction func pushToRegister(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let vc = storyboard.instantiateViewController(withIdentifier: "EnterIdViewController") as! EnterIdViewController
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
---
+++ instagramClone/ViewControllers/Register/EnterPhoneOrEmailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EnterPhoneOrEmailViewController.swift
+//  instagramClone
+//
+//  Created by 박현우 on 2022/06/20.
+//
+
+import UIKit
+
+class EnterPhoneOrEmailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+// MARK: - Extension
+private extension EnterPhoneOrEmailViewController {
+    
+}
